b3cf02745826c73c65dc6cd6054e5ac875378924
Beatriz Moura <beatrizmouradev@gmail.com>
2022-01-20 10:33:59 -0300

    projeto finalizdo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+//esse arquivo serve para configurar suas customizações
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: { 
+        corFundo: '#16174D',
+        amarelo: '#DDC726',
+        lilas: '#9082EC'
+      },
+      fontFamily: {
+        body: ['Lato', 'sans-serif']
+
+    }
+    },
+  },
+  plugins: [],
+}
